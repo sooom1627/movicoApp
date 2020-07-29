973015225459e94c91b45b4428a7d1fdc969fdd0
sooom1627 <swfh2367@gmail.con>
2020-07-29 11:34:05 +0900

    header

--- components/request/request.js
+++ components/request/request.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "react-native";
 
 import HomeScreen from "./newRequest/newRequest.js";
 import RequestDetail from "./requestDetail/requestDetail";
